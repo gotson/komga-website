bef09c5ffacb7d33f76dbfa53f48ad4e8751d6aa
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-07-10 10:42:35 +0800

    docs: fix aloglia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -165,19 +165,19 @@ const config = {
                 contextualSearch: true,
 
                 // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-                externalUrlRegex: 'external\\.com|domain\\.com',
+                // externalUrlRegex: 'external\\.com|domain\\.com',
 
                 // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-                replaceSearchResultPathname: {
-                    from: '/docs/', // or as RegExp: /\/docs\//
-                    to: '/',
-                },
+                // replaceSearchResultPathname: {
+                //     from: '/docs/', // or as RegExp: /\/docs\//
+                //     to: '/',
+                // },
 
                 // Optional: Algolia search parameters
                 searchParameters: {},
 
                 // Optional: path for search page that enabled by default (`false` to disable it)
-                searchPagePath: 'search',
+                searchPagePath: false,
 
                 //... other Algolia params
             },
