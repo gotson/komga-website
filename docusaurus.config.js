b795250dd4786264153f55f4f0c1ca7decefbdbd
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-07-10 09:43:07 +0800

    docs: disable show last update author

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     editUrl: 'https://github.com/gotson/komga-website/tree/master/',
-                    showLastUpdateAuthor: true,
+                    showLastUpdateAuthor: false,
                     showLastUpdateTime: true,
                 },
                 blog: {
