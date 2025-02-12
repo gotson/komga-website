1acd028085e214b822dc946a9af2f8fd79991f33
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-02-12 10:14:51 +0800

    build: fix mock-api-docs

--- website/emptySidebar.js
+++
@@ -1,3 +0,0 @@
-const emptySidebar = []
-
-export default emptySidebar
---
+++ website/mockSidebar.js
@@ -0,0 +1,7 @@
+const mockSidebar = [{
+    type: 'doc',
+    label: 'Introduction',
+    id: 'api/rest',
+}]
+
+export default mockSidebar
