2e8375dec4f6f971b3530b116039e765a250f058
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-02-25 11:02:08 +0800

    build: upgrade Docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -24,7 +24,12 @@ const config: Config = {
     projectName: 'komga-website', // Usually your repo name.
 
     onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
+
+    markdown: {
+        hooks: {
+            onBrokenMarkdownLinks: 'warn',
+        }
+    },
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
