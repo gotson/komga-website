6fabed8cddbf2aa31ead9a7ea221ae429bf9ccda
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-02-25 10:52:50 +0800

    i18n: enable croatian

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
     // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
-        locales: ['en'],
+        locales: ['en', 'hr'],
     },
 
     presets: [
